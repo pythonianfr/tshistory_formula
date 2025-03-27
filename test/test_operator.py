80278663c273e0f206cc7a3b0327d3844e550558
Aurélien Campéas <aurelien.campeas@pythonian.fr>
2025-03-27 16:59:42 +0100

    test/constant: exhibit failure of .interval with a constant series
    related to #44

--- test/test_operator.py
+++ test/test_operator.py
@@ -3154,6 +3154,8 @@ def test_constant(engine, tsh):
     )
 
     assert tsh.tzaware(engine, 'constant-1')
+    with pytest.raises(ValueError):
+        tsh.interval(engine, 'constant-1')
 
     ts = tsh.get(engine, 'constant-1')
     assert_df("""
